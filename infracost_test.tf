87f2d37281f9d76f36daee630d01a9e05504a8c5
DWeloo <dominiq.wiacek@o2.pl>
2025-12-01 04:35:31 +0100

    update resource network for infracost

--- infracost_test.tf
+++ infracost_test.tf
@@ -1,3 +1,12 @@
+resource "google_compute_network" "infracost_net" {
+  name                    = "infracost-net"
+  auto_create_subnetworks = true
+}
+
+# checkov:skip=CKV_GCP_39: test instance for infracost
+# checkov:skip=CKV_GCP_40: public IP allowed for test
+# checkov:skip=CKV_GCP_32: project-wide SSH keys allowed for test
+# checkov:skip=CKV_GCP_38: no CSEK for this non-production test instance
 resource "google_compute_instance" "my_instance" {
   zone = "europe-west1-b"
   name = "infracost-test"
@@ -5,13 +14,14 @@ resource "google_compute_instance" "my_instance" {
   machine_type = "n1-standard-16"
 
   network_interface {
-    network = "default"
+    network = google_compute_network.infracost_net.self_link
     access_config {}
   }
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "projects/debian-cloud/global/images/family/debian-11"
     }
   }
 }
+

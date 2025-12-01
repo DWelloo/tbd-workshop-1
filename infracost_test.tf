f9b76418b26d4c7dbd89962127b30d77ae019b2c
DWeloo <dominiq.wiacek@o2.pl>
2025-12-01 09:32:04 +0100

    Fix checkov warnings

--- infracost_test.tf
+++ infracost_test.tf
@@ -2,12 +2,10 @@ resource "google_compute_network" "infracost_net" {
   name                    = "infracost-net"
   auto_create_subnetworks = true
 }
-
 # checkov:skip=CKV_GCP_40: Public IP allowed for infracost test instance
 # checkov:skip=CKV_GCP_32: Project-wide SSH keys not relevant for test instance
 # checkov:skip=CKV_GCP_38: No customer-supplied encryption key needed for test instance
 # checkov:skip=CKV_GCP_39: Shielded VM not required for test instance
-
 resource "google_compute_instance" "my_instance" {
   zone = "europe-west1-b"
   name = "infracost-test"
